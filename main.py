ac603ecb85595ebc475b817ddfc83853d6dc4bea
Shayan Valaie <shayan@Shayans-MacBook-Pro.local>
2021-09-07 20:32:05 -0700

    added for loop to automated contact list email

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import smtplib
 from email.message import EmailMessage
 
-contacts = ["example1@gmail.com", "example2@gmail.com", "example3@gmail.com"]
+
+def send_email(contacts):
+    for contact in contacts:
+        msg = EmailMessage()
+        msg['Subject'] = "Email Subject Goes here"
+        msg['From'] = "yourEmail@gmail.com"
+        msg['To'] = contacts[contact]
+        msg.set_content('Example text body '
+                        'Example text body '
+                        'Example text body ')
 
 
-msg = EmailMessage()
-msg['Subject'] = "Subject"
-msg['From'] = 'sender@gmail.com'
-msg['To'] = "example1@gmail.com"
-msg.set_content('This is a message')
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login('yourEmail@gmail.com', 'yourEmailPassword')
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.login('SENDER EMAIL', 'PASSWORD')
-
-    smtp.send_message(msg)
+            smtp.send_message(msg)
 
 
+contacts = {
+
+    "ExampleName": "exampleEmail@gmail.com",
+
+
+}
+
+send_email(contacts)
